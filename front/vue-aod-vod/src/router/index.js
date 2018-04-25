6dd4e4bc7778d3a3fdd8395756ec890ce1718186
carlosyin <4870326@qq.com>
2018-04-25 19:49:49 +0800

    修改路由加载方式

--- front/vue-aod-vod/src/router/index.js
+++ front/vue-aod-vod/src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+Vue.use(Router);
+
 const HelloWorld = () => import('@/components/HelloWorld');
 const Index2 = () => import('@/components/Index2');
-
-
-Vue.use(Router);
 
 export default new Router({
   routes: [
