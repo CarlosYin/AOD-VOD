a51e8daeece1518d23a2d23b52c52523593c9fc3
carlosyin <4870326@qq.com>
2018-04-25 17:44:19 +0800

    新增路由懒加载

--- front/vue-aod-vod/src/router/index.js
+++ front/vue-aod-vod/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+
+const HelloWorld = () => import('@/components/HelloWorld');
+const Index2 = () => import('@/components/Index2');
+
 
 Vue.use(Router);
 
@@ -10,6 +13,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+      meta: {
+        requireAuth: false,
+      },
+    },
+    {
+      path: '/index2',
+      name: 'index2',
+      component: Index2,
+      meta: {
+        requireAuth: false,
+      },
     },
   ],
 });
