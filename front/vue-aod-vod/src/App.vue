7afb5c9dd3da185cbaad7862b9cb353518f73a63
carlosyin <4870326@qq.com>
2018-04-25 19:49:04 +0800

    修改样式

--- front/vue-aod-vod/src/App.vue
+++ front/vue-aod-vod/src/App.vue
@@ -14,14 +14,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 .router-fade-enter-active,
 .router-fade-leave-active {
   transition: opacity 0.2s;
