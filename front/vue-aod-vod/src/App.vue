71e5cdfe60e6eb55f0c40b6b15502d344293f017
carlosyin <4870326@qq.com>
2018-04-25 17:44:49 +0800

    新增路由切换动画

--- front/vue-aod-vod/src/App.vue
+++ front/vue-aod-vod/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <transition name="router-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -19,5 +21,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.router-fade-enter-active,
+.router-fade-leave-active {
+  transition: opacity 0.2s;
+}
+.router-fade-enter,
+.router-fade-leave-active {
+  opacity: 0;
 }
 </style>
